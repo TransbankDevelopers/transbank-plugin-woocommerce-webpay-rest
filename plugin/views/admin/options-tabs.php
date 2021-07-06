a2328db8639059430a049e7dfdc61453e9d38e85
Gonzalo De Spirito <gonzunigad@users.noreply.github.com>
2021-07-06 18:42:05 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- plugin/views/admin/options-tabs.php
+++ plugin/views/admin/options-tabs.php
@@ -10,40 +10,40 @@ if (!defined('ABSPATH')) {
     <h2 class="nav-tab-wrapper">
         <a href="<?php echo admin_url('admin.php?page=wc-settings&tab=checkout&section=transbank_webpay_plus_rest&tbk_tab=options') ?>"
            class="nav-tab <?php if ($tab === 'options') {
-               echo 'nav-tab-active';
-           } ?>">Webpay Plus</a>
+    echo 'nav-tab-active';
+} ?>">Webpay Plus</a>
         <a href="<?php echo admin_url('admin.php?page=wc-settings&tab=checkout&section=transbank_oneclick_mall_rest&tbk_tab=options_oneclick') ?>"
            class="nav-tab <?php if ($tab === 'options_oneclick') {
-               echo 'nav-tab-active';
-           } ?>">Webpay Oneclick</a>
+    echo 'nav-tab-active';
+} ?>">Webpay Oneclick</a>
         <a href="<?php echo admin_url('admin.php?page=transbank_webpay_plus_rest&tbk_tab=healthcheck') ?>"
            class="nav-tab <?php if ($tab === 'healthcheck') {
-               echo 'nav-tab-active';
-           } ?>">Diagnóstico</a>
+    echo 'nav-tab-active';
+} ?>">Diagnóstico</a>
         <a href="<?php echo admin_url('admin.php?page=transbank_webpay_plus_rest&tbk_tab=logs') ?>"
            class="nav-tab <?php if ($tab === 'logs') {
-               echo 'nav-tab-active';
-           } ?>">Registros (logs)</a>
+    echo 'nav-tab-active';
+} ?>">Registros (logs)</a>
         <a href="<?php echo admin_url('admin.php?page=transbank_webpay_plus_rest&tbk_tab=phpinfo') ?>"
            class="nav-tab <?php if ($tab === 'phpinfo') {
-               echo 'nav-tab-active';
-           } ?>">PHP Info</a>
+    echo 'nav-tab-active';
+} ?>">PHP Info</a>
     </h2>
 
 
     <div class="content">
         <?php
         if ($tab === 'options') {
-            include __DIR__ . '/admin-options.php';
+            include __DIR__.'/admin-options.php';
         } elseif ($tab === 'options_oneclick') {
-            include __DIR__ . '/oneclick-admin-options.php';
+            include __DIR__.'/oneclick-admin-options.php';
         } elseif ($tab === 'logs') {
-            include __DIR__ . '/logs.php';
+            include __DIR__.'/logs.php';
         } elseif ($tab === 'phpinfo') {
-            include __DIR__ . '/phpinfo.php';
+            include __DIR__.'/phpinfo.php';
         } else {
             $datos_hc = json_decode($healthcheck->printFullResume());
-            include __DIR__ . '/healthcheck.php';
+            include __DIR__.'/healthcheck.php';
         }
         ?>
     </div>
