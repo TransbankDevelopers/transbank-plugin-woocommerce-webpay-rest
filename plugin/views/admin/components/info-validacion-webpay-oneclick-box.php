d3f4e54804a4d462f4fa32532079127c135e9334
Gonzalo De Spírito <gonzunigad@gmail.com>
2021-07-26 14:23:54 -0400

    Fix typo

--- plugin/views/admin/components/info-validacion-webpay-oneclick-box.php
+++ plugin/views/admin/components/info-validacion-webpay-oneclick-box.php
@@ -5,7 +5,7 @@
     <h4>Código de comercio</h4>
     Si no lo tienes, puedes solicitarlo en <a target="_blank" href="https://contrata.transbankdevelopers.cl/Oneclick/?wpcommerce=<?php echo $webpayPlusCommerceCode; ?>&wpenv=<?php echo $webpayPlusEnvironment; ?>&utm_source=woocommerce_plugin&utm_medium=banner&utm_campaign=contrata">usando este formulario</a>.
 
-    <h4>Tu Api Key: Proceo de validación</h4>
+    <h4>Tu Api Key: Proceso de validación</h4>
     Si ya tienes tus códigos de comercio, lo único que te faltaría es tu Api Key. Para obtenerla, debes completar el siguiente formulario:
     <br>
     <a href="https://form.typeform.com/to/fZqOJyFZ?typeform-medium=embed-snippet" style="margin-top: 5px; display: inline-block;clear: both" data-mode="popup" class="typeform-share link button-primary" data-size="100" data-submit-close-delay="25">Comenzar proceso de validación</a>
