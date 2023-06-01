b611c60ffac5377bcfbe1e5d0a8c332e1b1b45b8
Miguel Vargas <mvarlic@gmail.com>
2023-06-01 12:49:45 -0500

    add new webpay exceptions

---
+++ plugin/src/Exceptions/Webpay/UserCancelWebpayException.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Transbank\WooCommerce\WebpayRest\Exceptions\Webpay;
+
+class UserCancelWebpayException extends \Exception
+{
+    private $tbkToken;
+    private $transaction;
+
+    public function __construct($message, $tbkToken, $transaction, $code = 0, \Exception $previous = null) {
+        $this->tbkToken = $tbkToken;
+        $this->transaction = $transaction;
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getTbkToken() {
+        return $this->tbkToken;
+    }
+
+    public function getTransaction() {
+        return $this->transaction;
+    }
+}
---
+++ plugin/src/Exceptions/Webpay/WithoutTokenWebpayException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Transbank\WooCommerce\WebpayRest\Exceptions\Webpay;
+
+class WithoutTokenWebpayException extends \Exception
+{
+
+}
