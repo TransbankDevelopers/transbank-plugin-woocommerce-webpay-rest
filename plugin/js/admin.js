b7387aed90bb21b476caab5ee9b2e66cc2ba89db
Miguel Vargas <mvarlic@gmail.com>
2023-06-21 15:29:00 -0500

    update test call

--- plugin/js/admin.js
+++ plugin/js/admin.js
@@ -12,18 +12,19 @@ jQuery(function($) {
     $("#row_error_detail").hide();
     $(".tbk_table_trans").empty();
 
-    $.post(ajax_object.ajax_url, {action: 'check_connection', nonce: ajax_object.nonce}, function(response){
+    $.post(ajax_object.ajax_url, {action: 'check_connection', nonce: ajax_object.nonce}, function(resp){
+      console.log(resp);
       $(".check_conn").text("Verificar Conexión");
       $('.tbk-response-title').show();
       $("#response_title").show();
       $("#row_response_status").show();
       $("#row_response_status_text").removeClass("label-success").removeClass("label-danger");
 
-      if(response.status.string == "OK") {
+      if(resp.status.string == "OK") {
 
         $("#row_response_status_text").addClass("label-success").text("OK").show();
-        $("#row_response_url_text").append(response.response.url);
-        $("#row_response_token_text").append('<pre>'+response.response.token_ws+'</pre>');
+        $("#row_response_url_text").append(resp.response.url);
+        $("#row_response_token_text").append('<pre>'+resp.response.token+'</pre>');
 
         $("#row_response_url").show();
         $("#row_response_token").show();
@@ -31,8 +32,8 @@ jQuery(function($) {
       } else {
 
         $("#row_response_status_text").addClass("label-danger").text("ERROR").show();
-        $("#row_error_message_text").append(response.response.error);
-        $("#row_error_detail_text").append('<code style="display: block; padding: 10px">'+response.response.detail+'</code>');
+        $("#row_error_message_text").append(resp.response.error);
+        $("#row_error_detail_text").append('<code style="display: block; padding: 10px">'+resp.response.detail+'</code>');
 
         $("#row_error_message").show();
         $("#row_error_detail").show();
@@ -76,9 +77,9 @@ jQuery(function($) {
 
       }.bind(this))
       .fail(function(e, a) {
-          alert('Falló la consulta de cuotas: ' + e.responseJSON.message)
-          $(this).data('sending', false);
-          $(this).html('Consultar estado de la transacción');
+        alert('Falló la consulta de cuotas: ' + e.responseJSON.message)
+        $(this).data('sending', false);
+        $(this).html('Consultar estado de la transacción');
       })
   });
 
@@ -118,6 +119,3 @@ jQuery(function($) {
   });
 
 })
-
-
-
