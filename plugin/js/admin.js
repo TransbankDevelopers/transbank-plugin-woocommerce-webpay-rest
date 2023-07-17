309dd35d31818867bf5978616411da6256a095e3
Isabel Anabalón Fuentes <isabel.anabalonfuentes@gmail.com>
2023-07-17 13:55:03 -0400

    refactor: replace tables by grid on healthcheck.php
    refactor: replace tables by grid on healthcheck.php

--- plugin/js/admin.js
+++ plugin/js/admin.js
@@ -1,117 +1,103 @@
 jQuery(function($) {
-  $(".check_conn").on("click",function(e) {
+    $(".check_conn").on("click",function(e) {
 
-    $(".check_conn").text("Verificando ...");
-    $("#response_title").hide();
-    $("#row_response_status").hide();
-    $("#row_response_url").hide();
-    $("#row_response_token").hide();
-    $("#row_error_message").hide();
-    $("#row_error_detail").hide();
-    $(".tbk_table_trans").empty();
+        $(".check_conn").text("Verificando ...");
 
-    $.post(ajax_object.ajax_url, {action: 'check_connection', nonce: ajax_object.nonce}, function(resp){
-      $(".check_conn").text("Verificar Conexión");
-      $('.tbk-response-title').show();
-      $("#response_title").show();
-      $("#row_response_status").show();
-      $("#row_response_status_text").removeClass("label-success").removeClass("label-danger");
+        $("#tbk_response_status").removeClass("tbk-hide");
+        $("#div_status_ok").addClass("tbk-hide");
+        $("#div_status_error").addClass("tbk-hide");
 
-      if(resp.status.string == "OK") {
-
-        $("#row_response_status_text").addClass("label-success").text("OK").show();
-        $("#row_response_url_text").append(resp.response.url);
-        $("#row_response_token_text").append('<pre>'+resp.response.token+'</pre>');
-
-        $("#row_response_url").show();
-        $("#row_response_token").show();
-
-      } else {
-
-        $("#row_response_status_text").addClass("label-danger").text("ERROR").show();
-        $("#row_error_message_text").append(resp.response.error);
-        $("#row_error_detail_text").append('<code style="display: block; padding: 10px">'+resp.response.detail+'</code>');
-
-        $("#row_error_message").show();
-        $("#row_error_detail").show();
-      }
-
-    })
-
-    e.preventDefault();
-  });
-
-  $('.get-transaction-status').on("click",function(e) {
-      if ($(this).data('sending') === true) {
-          return;
-      }
-      $(this).data('sending', true);
-      $(this).html('Consultando al API REST...');
-     e.preventDefault();
-
-      $.post(ajax_object.ajax_url, {
-          action: 'get_transaction_status',
-          order_id: $(this).data('order-id'),
-          buy_order: $(this).data('buy-order'),
-          token: $(this).data('token'),
-          nonce: window.ajax_object.nonce
-      }, function(response){
-          let $table = $('.transaction-status-response');
-          let statusData = response.status;
-          Object.keys(statusData).forEach(key => {
-              let value = statusData[key] ? statusData[key] : '-';
-              $table.find('.status-' + key).html(value);
-          });
-          $table.find('.status-product').html(response.product);
-
-          let niceJson = JSON.stringify(response.raw, null, 2)
-          $table.find('.status-raw').html(`<pre>${niceJson}</pre>`);
-          $table.show();
-          $(this).data('sending', false);
-          $(this).html('Consultar estado de la transacción');
-
-      }.bind(this))
-      .fail(function(e, a) {
-          $('.error-status-raw').html(`<p>${e.responseJSON.message}</p>`);
-          $('.error-transaction-status-response').show();
-          $(this).data('sending', false);
-          $(this).html('Consultar estado de la transacción');
-      })
-  });
+        $.post(ajax_object.ajax_url, {action: 'check_connection', nonce: ajax_object.nonce}, function(response){
+            $(".check_conn").text("Verificar Conexión");
 
 
-  $(".check_exist_tables").on("click",function(e) {
+            if(response.status.string == "OK") {
+                $("#response_url_text").text(response.response.url);
+                $("#response_token_text").html('<pre>'+response.response.token+'</pre>');
 
-    $(".check_exist_tables").text("Verificando ...");
-    $("#row_tbl_response_status").hide();
-    $("#row_tbl_response_result").hide();
-    $("#row_tbl_error_message").hide();
-    $('.tbk-tbl-response-title').show();
-    $(".tbk_tbl_table_trans").empty();
+                $("#div_status_ok").removeClass("tbk-hide");
 
-    $.post(ajax_object.ajax_url, {action: 'check_exist_tables', nonce: ajax_object.nonce}, function(response){
-      $(".check_exist_tables").text("Verificar Tablas");
-      $("#row_tbl_response_status").show();
-      $("#row_tbl_response_status_text").removeClass("label-success").removeClass("label-danger");
+            } else {
+                $("#error_response_text").text(response.response.error);
+                $("#error_detail_response_text").html('<code style="display: block; padding: 10px">'+response.response.detail+'</code>');
 
-      if(!response.error) {
+                $("#div_status_error").removeClass("tbk-hide");
+            }
 
-        $("#row_tbl_response_status_text").addClass("label-success").text("OK").show();
-        $("#row_tbl_response_result_text").append(response.msg);
+        })
 
-        $("#row_tbl_response_result").show();
+        e.preventDefault();
+    });
 
-      } else {
+    $('.get-transaction-status').on("click",function(e) {
+        if ($(this).data('sending') === true) {
+            return;
+        }
+        $(this).data('sending', true);
+        $(this).html('Consultando al API REST...');
+        e.preventDefault();
 
-        $("#row_tbl_response_status_text").addClass("label-danger").text("ERROR").show();
-        $("#row_tbl_error_message_text").append('<code style="display: block; padding: 10px">' + response.error + ' Exception: ' + response.exception +'</code>');
+        $.post(ajax_object.ajax_url, {
+            action: 'get_transaction_status',
+            order_id: $(this).data('order-id'),
+            buy_order: $(this).data('buy-order'),
+            token: $(this).data('token'),
+            nonce: window.ajax_object.nonce
+        }, function(response){
+            let $table = $('.transaction-status-response');
+            let statusData = response.status;
+            Object.keys(statusData).forEach(key => {
+                let value = statusData[key] ? statusData[key] : '-';
+                $table.find('.status-' + key).html(value);
+            });
+            $table.find('.status-product').html(response.product);
 
-        $("#row_tbl_error_message").show();
-      }
+            let niceJson = JSON.stringify(response.raw, null, 2)
+            $table.find('.status-raw').html(`<pre>${niceJson}</pre>`);
+            $table.show();
+            $(this).data('sending', false);
+            $(this).html('Consultar estado de la transacción');
 
-    })
+        }.bind(this))
+            .fail(function(e, a) {
+                $('.error-status-raw').html(`<p>${e.responseJSON.message}</p>`);
+                $('.error-transaction-status-response').show();
+                $(this).data('sending', false);
+                $(this).html('Consultar estado de la transacción');
+            })
+    });
 
-    e.preventDefault();
-  });
+
+    $(".check_exist_tables").on("click",function(e) {
+        $(".check_exist_tables").text("Verificando ...");
+        $("#tbk-tbl-response-title").removeClass("tbk-hide");
+        $("#div_tables_status").addClass("tbk-hide");
+        $("#div_tables_status_result").addClass("tbk-hide");
+        $("#div_tables_error").addClass("tbk-hide");
+        $("#tbl_response_status_text").removeClass();
+
+        $.post(ajax_object.ajax_url, {action: 'check_exist_tables', nonce: ajax_object.nonce}, function(response){
+            $(".check_exist_tables").text("Verificar Tablas");
+            $("#div_tables_status").removeClass("tbk-hide");
+
+            if(!response.error) {
+
+                $("#tbl_response_status_text").addClass("label-success").text("OK").show();
+                $("#tbl_response_result_text").text(response.msg);
+
+                $("#div_tables_status_result").removeClass("tbk-hide");
+
+            } else {
+
+                $("#tbl_response_status_text").addClass("label-danger").text("ERROR").show();
+                $("#tbl_error_message_text").html('<code style="display: block; padding: 10px">' + response.error + ' Exception: ' + response.exception +'</code>');
+
+                $("#div_tables_error").removeClass("tbk-hide");
+            }
+
+        })
+
+        e.preventDefault();
+    });
 
 })
