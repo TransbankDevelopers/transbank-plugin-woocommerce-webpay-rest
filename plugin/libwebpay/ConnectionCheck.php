00f0ced76023c8ae6261afb89a19c432101a9da2
Miguel Vargas <mvarlic@gmail.com>
2023-10-31 13:31:22 -0500

    refactor: replace HealthCheck class by method

--- plugin/libwebpay/ConnectionCheck.php
+++ plugin/libwebpay/ConnectionCheck.php
@@ -1,18 +1,43 @@
 <?php
 
-use Transbank\WooCommerce\WebpayRest\Helpers\HealthCheckFactory;
+use Transbank\WooCommerce\WebpayRest\WebpayplusTransbankSdk;
 
 class ConnectionCheck
 {
     public static function check()
     {
-        $healthCheck = HealthCheckFactory::create();
-
-        $resp = $healthCheck->setCreateTransaction();
+        $resp = ConnectionCheck::setCreateTransaction();
 
         header('Content-Type: application/json');
         ob_clean();
         echo json_encode($resp);
         wp_die();
+    }
+
+    public static function setCreateTransaction()
+    {
+        $amount = 990;
+        $buyOrder = '_Healthcheck_';
+        $sessionId = uniqid();
+        $returnUrl = 'http://test.com/test';
+
+        $status = 'Error';
+        try {
+            $webpayplusTransbankSdk = new WebpayplusTransbankSdk();
+            $result = $webpayplusTransbankSdk->createInner(0, $buyOrder, $sessionId, $amount, $returnUrl);
+            $status = 'OK';
+
+        } catch (\Exception $e) {
+            $status = 'Error';
+            $result = [
+                'error'  => 'Error al crear la transacción',
+                'detail' => $e->getMessage()
+            ];
+        }
+
+        return [
+            'status'   => ['string' => $status],
+            'response' => $result
+        ];
     }
 }
