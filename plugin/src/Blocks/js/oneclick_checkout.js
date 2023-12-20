4ff086e28947558fc9928953ee341320899de6c3
Matias Vasquez <m.vasquezac@gmail.com>
2023-12-20 01:53:55 -0300

    feat: add client script for oneclick

---
+++ plugin/src/Blocks/js/oneclick_checkout.js
@@ -0,0 +1,39 @@
+import { registerPaymentMethod } from '@woocommerce/blocks-registry';
+import { decodeEntities } from '@wordpress/html-entities';
+import { getSetting } from '@woocommerce/settings';
+
+const settings = getSetting( 'transbank_oneclick_mall_rest_data', {} );
+
+const label = decodeEntities( settings.title );
+
+const Content = () => {
+	return decodeEntities( settings.description );
+};
+
+const Label = () => {
+	const title = decodeEntities( settings.title );
+	const imagePath = settings.icon;
+	const paymentImage = (
+		<img src={imagePath} />
+	);
+	return (
+		<div>
+			{title}
+			{paymentImage}
+		</div>
+	);
+};
+
+const TransbankOneclickBlocks = {
+	name: "transbank_oneclick_mall_rest",
+	label: <Label />,
+	content: <Content />,
+	edit: <Content />,
+	canMakePayment: () => true,
+	ariaLabel: label,
+	supports: {
+		features: settings.supports,
+	},
+};
+
+registerPaymentMethod( TransbankOneclickBlocks );
