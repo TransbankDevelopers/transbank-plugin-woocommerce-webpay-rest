4857ce29a171e81ac336413f79cac3230db16d53
Matias Vasquez <m.vasquezac@gmail.com>
2023-12-20 01:54:19 -0300

    feat: add client script for webpay

---
+++ plugin/src/Blocks/js/webpay_checkout.js
@@ -0,0 +1,39 @@
+import { registerPaymentMethod } from '@woocommerce/blocks-registry';
+import { decodeEntities } from '@wordpress/html-entities';
+import { getSetting } from '@woocommerce/settings';
+
+const settings = getSetting( 'transbank_webpay_plus_rest_data', {} );
+
+const label = decodeEntities( settings.title );
+
+const Content = () => {
+	return decodeEntities( settings.description );
+};
+
+const Label = () => {
+	const title = decodeEntities( settings.title );
+	const imagePath = settings.icon;
+	const paymentImage = (
+		<img src={imagePath} />
+	);
+	return (
+		<div>
+			{title}
+			{paymentImage}
+		</div>
+	);
+};
+
+const TransbankWebpayBlocks = {
+	name: "transbank_webpay_plus_rest",
+	label: <Label />,
+	content: <Content />,
+	edit: <Content />,
+	canMakePayment: () => true,
+	ariaLabel: label,
+	supports: {
+		features: settings.supports,
+	},
+};
+
+registerPaymentMethod( TransbankWebpayBlocks );
