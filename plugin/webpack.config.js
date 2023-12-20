6177dcac340210d4d2c6d655fc4fab281df70302
Matias Vasquez <m.vasquezac@gmail.com>
2023-12-20 02:04:24 -0300

    feat: add webpack config

---
+++ plugin/webpack.config.js
@@ -0,0 +1,48 @@
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const WooCommerceDependencyExtractionWebpackPlugin = require('@woocommerce/dependency-extraction-webpack-plugin');
+const path = require('path');
+
+const wcDepMap = {
+	'@woocommerce/blocks-registry': ['wc', 'wcBlocksRegistry'],
+	'@woocommerce/settings'       : ['wc', 'wcSettings']
+};
+
+const wcHandleMap = {
+	'@woocommerce/blocks-registry': 'wc-blocks-registry',
+	'@woocommerce/settings'       : 'wc-settings'
+};
+
+const requestToExternal = (request) => {
+	if (wcDepMap[request]) {
+		return wcDepMap[request];
+	}
+};
+
+const requestToHandle = (request) => {
+	if (wcHandleMap[request]) {
+		return wcHandleMap[request];
+	}
+};
+
+// Export configuration.
+module.exports = {
+	...defaultConfig,
+	entry: 
+		{'webpay_blocks': '/src/Blocks/js/webpay_checkout.js',
+		'oneclick_blocks': '/src/Blocks/js/oneclick_checkout.js'}
+	,
+	output: {
+		path: path.resolve( __dirname, 'js/front' ),
+		filename: '[name].js',
+	},
+	plugins: [
+		...defaultConfig.plugins.filter(
+			(plugin) =>
+				plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
+		),
+		new WooCommerceDependencyExtractionWebpackPlugin({
+			requestToExternal,
+			requestToHandle
+		})
+	]
+};
