f71487e6e21ad202772501d5fba340c8e4cc57d9
Matias Vasquez <m.vasquezac@gmail.com>
2023-12-26 16:46:09 -0300

    refactor: use trait on webpayblocks gateway

--- plugin/src/Blocks/WC_Gateway_Transbank_Webpay_Blocks.php
+++ plugin/src/Blocks/WC_Gateway_Transbank_Webpay_Blocks.php
@@ -1,48 +1,18 @@
 <?php
 
+namespace Transbank\WooCommerce\WebpayRest\Blocks;
 use Automattic\WooCommerce\Blocks\Payments\Integrations\AbstractPaymentMethodType;
 
 final class WCGatewayTransbankWebpayBlocks extends AbstractPaymentMethodType {
-    private $gateway;
+
+    use WCGatewayTransbankBlocks;
+
     protected $name = 'transbank_webpay_plus_rest';
 
-    public function initialize() {
-        $this->settings = get_option('transbank_webpay_plus_rest_settings', []);
-        $this->gateway = $this->getGateway();
-    }
-
-    public function is_active() {
-        return $this->gateway->is_available();
-    }
-
-    public function get_payment_method_script_handles() {
+    public function __construct() {
         $scriptPath = dirname(dirname(plugin_dir_path(__FILE__))) . '/js/front/';
-        $scriptInfo = require_once $scriptPath . 'webpay_blocks.asset.php';
-        wp_register_script(
-            'wc_transbank_webpay_payment',
-            dirname(dirname(plugins_url('/', __FILE__))) . '/js/front/webpay_blocks.js',
-            $scriptInfo['dependencies'],
-            $scriptInfo['version'],
-            true
-        );
-
-        return['wc_transbank_webpay_payment'];
-    }
-
-    public function get_payment_method_data() {
-        return [
-            'title' => $this->gateway->title,
-            'description' => $this->gateway->description,
-            'supports' => $this->gateway->supports,
-            'icon' => $this->gateway->icon
-        ];
-    }
-
-    private function getGateway() {
-        $gateways = WC()->payment_gateways->get_available_payment_gateways();
-        if(isset($gateways[$this->name])) {
-            return $gateways[$this->name];
-        }
-        return null;
+        $this->scriptInfo = require_once $scriptPath . 'webpay_blocks.asset.php';
+        $this->paymentId = $this->name;
+        $this->productName = 'webpay';
     }
 }
