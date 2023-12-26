759e37e5b4ec0f087d9602c65009712e3d923429
Matias Vasquez <m.vasquezac@gmail.com>
2023-12-26 16:46:36 -0300

    refactor: use trait on oneclick blocks gateway

--- plugin/src/Blocks/WC_Gateway_Transbank_Oneclick_Blocks.php
+++ plugin/src/Blocks/WC_Gateway_Transbank_Oneclick_Blocks.php
@@ -1,48 +1,18 @@
 <?php
 
+namespace Transbank\WooCommerce\WebpayRest\Blocks;
 use Automattic\WooCommerce\Blocks\Payments\Integrations\AbstractPaymentMethodType;
 
 final class WCGatewayTransbankOneclickBlocks extends AbstractPaymentMethodType {
-    private $gateway;
+
+    use WCGatewayTransbankBlocks;
+
     protected $name = 'transbank_oneclick_mall_rest';
 
-    public function initialize() {
-        $this->settings = get_option('transbank_oneclick_mall_rest_settings', []);
-        $this->gateway = $this->getGateway();
-    }
-
-    public function is_active() {
-        return $this->gateway->is_available();
-    }
-
-    public function get_payment_method_script_handles() {
+    public function __construct() {
         $scriptPath = dirname(dirname(plugin_dir_path(__FILE__))) . '/js/front/';
-        $scriptInfo = require_once $scriptPath . 'oneclick_blocks.asset.php';
-        wp_register_script(
-            'wc_transbank_oneclick_payment',
-            dirname(dirname(plugins_url('/', __FILE__))) . '/js/front/oneclick_blocks.js',
-            $scriptInfo['dependencies'],
-            $scriptInfo['version'],
-            true
-        );
-
-        return['wc_transbank_oneclick_payment'];
-    }
-
-    public function get_payment_method_data() {
-        return [
-            'title' => $this->gateway->title,
-            'description' => $this->gateway->description,
-            'supports' => $this->gateway->supports,
-            'icon' => $this->gateway->icon
-        ];
-    }
-
-    private function getGateway() {
-        $gateways = WC()->payment_gateways->get_available_payment_gateways();
-        if(isset($gateways[$this->name])) {
-            return $gateways[$this->name];
-        }
-        return null;
+        $this->scriptInfo = require_once $scriptPath . 'oneclick_blocks.asset.php';
+        $this->paymentId = $this->name;
+        $this->productName = 'oneclick';
     }
 }
