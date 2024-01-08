b421d4fc636950d0c7fcfce3f33ed17cfb740265
Matias Vasquez <m.vasquezac@gmail.com>
2024-01-08 18:10:53 -0300

    feat: add blocks helper class

---
+++ plugin/src/Helpers/BlocksHelper.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Transbank\WooCommerce\WebpayRest\Helpers;
+
+use Automattic\WooCommerce\Blocks\Utils\CartCheckoutUtils;
+
+class BlocksHelper
+{
+
+    const ONECLICK_SUCCESSFULL_INSCRIPTION = 0;
+    const ONECLICK_TIMEOUT = 1;
+    const ONECLICK_WITHOUT_TOKEN = 2;
+    const ONECLICK_USER_CANCELED = 3;
+    const ONECLICK_INVALID_STATUS = 4;
+    const ONECLICK_FINISH_ERROR = 5;
+    const ONECLICK_REJECTED_INSCRIPTION = 6;
+    const WEBPAY_APPROVED = 7;
+    const WEBPAY_ALREADY_PAID = 8;
+    const WEBPAY_INVALID_ORDER_STATUS = 9;
+    const WEBPAY_TIMEOUT = 10;
+    const WEBPAY_USER_CANCELED_ALREADY_PAID = 11;
+    const WEBPAY_USER_CANCELED = 12;
+    const WEBPAY_DOUBLE_TOKEN = 13;
+    const WEBPAY_INVALID_STATUS = 14;
+    const WEBPAY_REJECTED_COMMIT = 15;
+    const WEBPAY_COMMIT_ERROR = 16;
+    const WEBPAY_EXCEPTION = 17;
+
+    public function checkBlocksEnabled() {
+
+        if(class_exists( 'Automattic\WooCommerce\Blocks\Utils\CartCheckoutUtils' )  && CartCheckoutUtils::is_checkout_block_default()) {
+            return true;
+        }
+        return false;
+    }
+
+}
+?>
