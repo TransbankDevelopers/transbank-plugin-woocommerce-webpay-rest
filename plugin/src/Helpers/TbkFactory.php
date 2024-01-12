3da498fca47e4ee982ae13d16aa6f01b8a5965a6
Matias Vasquez <m.vasquezac@gmail.com>
2024-01-12 16:31:52 -0300

    feat: update logs location

--- plugin/src/Helpers/TbkFactory.php
+++ plugin/src/Helpers/TbkFactory.php
@@ -5,11 +5,13 @@ namespace Transbank\WooCommerce\WebpayRest\Helpers;
 use Transbank\Plugin\Helpers\PluginLogger;
 use Transbank\Plugin\Model\LogConfig;
 
+define('Transbank_webpay_Rest_Webpay_UPLOADS', untrailingslashit(wp_upload_dir()['basedir'] . '/transbank_webpay_plus_rest'));
+
 class TbkFactory
 {
     public static function createLogger()
     {
-        $config = new LogConfig(dirname(dirname(__DIR__)).'/logs');
+        $config = new LogConfig(Transbank_webpay_Rest_Webpay_UPLOADS .'/logs');
         return new PluginLogger($config);
     }
 }
