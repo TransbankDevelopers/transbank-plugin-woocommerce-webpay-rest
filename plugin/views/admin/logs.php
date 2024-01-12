a414364e841debcbbfa36b88969f728f594557ed
Matias Vasquez <m.vasquezac@gmail.com>
2024-01-12 16:33:48 -0300

    feat: hide log details when file not exists

--- plugin/views/admin/logs.php
+++ plugin/views/admin/logs.php
@@ -8,6 +8,7 @@ if (!defined('ABSPATH')) {
 $log = TbkFactory::createLogger();
 $resume = $log->getInfo();
 $lastLog = $log->getLogDetail(basename($resume['last']));
+$folderHasLogs = $resume['length'] > 0;
 ?>
 <div class="tbk-box">
     <div id="logs" class="tab-pane">
@@ -56,83 +57,86 @@ $lastLog = $log->getLogDetail(basename($resume['last']));
                 </span>
                 </div>
             </div>
-            <div class="tbk-plugin-info-container logs" id="div_logs_list">
-                <div class="info-column">
-                    <div title="Lista los archivos archivos que guardan la información de cada compra mediante Webpay"
-                         class="label label-info">?
+            <?php if ($folderHasLogs) { ?>
+                <div class="tbk-plugin-info-container logs" id="div_logs_list">
+                    <div class="info-column">
+                        <div title="Lista los archivos archivos que guardan la información de cada compra mediante Webpay"
+                            class="label label-info">?
+                        </div>
+                    </div>
+                    <div class="info-column-plugin log">
+                        <span class="highlight-text"> Listado de Registros Disponibles:  </span>
+                    </div>
+                    <div class="info-column-plugin log">
+                        <span class="label">
+                            <?php
+                                foreach ($resume['logs'] as $index) {
+                                    echo '<li>'.$index['filename'].'</li>';
+                                }
+                            ?>
+                        </span>
                     </div>
                 </div>
-                <div class="info-column-plugin log">
-                    <span class="highlight-text"> Listado de Registros Disponibles:  </span>
-                </div>
-                <div class="info-column-plugin log">
-                <span class="label">
-                   <?php
-                   foreach ($resume['logs'] as $index) {
-                       echo '<li>'.$index['filename'].'</li>';
-                   }
-                   ?>
-                </span>
-                </div>
-            </div>
+            <?php } ?>
         </div>
 
+        <?php if ($folderHasLogs) { ?>
+            <h3 class="tbk_title_h3">Últimos Registros</h3>
+            <div id="tbk-last-logs" >
+                <div class="tbk-plugin-info-container logs" id="div_last_log">
+                    <div class="info-column">
+                        <div title="Nombre del útimo archivo de registro creado"
+                            class="label label-info">?
+                        </div>
+                    </div>
+                    <div class="info-column-plugin">
+                        <span class="highlight-text"> Último Documento: </span>
+                    </div>
+                    <div class="info-column-plugin">
+                            <span class="label">
+                                <?php echo $lastLog['filename'] ?? '-'; ?>
+                            </span>
+                    </div>
+                </div>
+                <div class="tbk-plugin-info-container logs" id="div_size_log">
+                    <div class="info-column">
+                        <div title="Peso del último archivo de registro creado"
+                            class="label label-info">?
+                        </div>
+                    </div>
+                    <div class="info-column-plugin">
+                        <span class="highlight-text"> Peso del Documento: </span>
+                    </div>
+                    <div class="info-column-plugin">
+                            <span class="label">
+                                <?php echo $lastLog['size'] ?? '-'; ?>
+                            </span>
+                    </div>
+                </div>
+                <div class="tbk-plugin-info-container logs" id="div_lines_logs">
+                    <div class="info-column">
+                        <div title="Cantidad de líneas que posee el último archivo de registro creado"
+                            class="label label-info">?
+                        </div>
+                    </div>
+                    <div class="info-column-plugin">
+                        <span class="highlight-text"> Cantidad de Líneas: </span>
+                    </div>
+                    <div class="info-column-plugin">
+                            <span class="label">
+                                <?php echo $lastLog['lines'] ?? '-'; ?>
+                            </span>
+                    </div>
+                </div>
+            </div>
 
-        <h3 class="tbk_title_h3">Últimos Registros</h3>
-        <div id="tbk-last-logs" >
-            <div class="tbk-plugin-info-container logs" id="div_last_log">
-                <div class="info-column">
-                    <div title="Nombre del útimo archivo de registro creado"
-                         class="label label-info">?
-                    </div>
-                </div>
-                <div class="info-column-plugin">
-                    <span class="highlight-text"> Último Documento: </span>
-                </div>
-                <div class="info-column-plugin">
-                        <span class="label">
-                            <?php echo $lastLog['filename'] ?? '-'; ?>
-                        </span>
-                </div>
+            <div id="tbk_last_log_content">
+                <pre>
+                    <span style="font-size: 10px; padding: 5px; font-family:monospace; display: block; background: white;">
+                        <?php echo $lastLog['content'] ?? '-'; ?>
+                    </span>
+                </pre>
             </div>
-            <div class="tbk-plugin-info-container logs" id="div_size_log">
-                <div class="info-column">
-                    <div title="Peso del último archivo de registro creado"
-                         class="label label-info">?
-                    </div>
-                </div>
-                <div class="info-column-plugin">
-                    <span class="highlight-text"> Peso del Documento: </span>
-                </div>
-                <div class="info-column-plugin">
-                        <span class="label">
-                            <?php echo $lastLog['size'] ?? '-'; ?>
-                        </span>
-                </div>
-            </div>
-            <div class="tbk-plugin-info-container logs" id="div_lines_logs">
-                <div class="info-column">
-                    <div title="Cantidad de líneas que posee el último archivo de registro creado"
-                         class="label label-info">?
-                    </div>
-                </div>
-                <div class="info-column-plugin">
-                    <span class="highlight-text"> Cantidad de Líneas: </span>
-                </div>
-                <div class="info-column-plugin">
-                        <span class="label">
-                            <?php echo $lastLog['lines'] ?? '-'; ?>
-                        </span>
-                </div>
-            </div>
-        </div>
-
-        <div id="tbk_last_log_content">
-            <pre>
-            <span style="font-size: 10px; padding: 5px; font-family:monospace; display: block; background: white;">
-                <?php echo $lastLog['content'] ?? '-'; ?>
-            </span>
-        </pre>
-        </div>
+        <?php } ?>
     </div>
 </div>
