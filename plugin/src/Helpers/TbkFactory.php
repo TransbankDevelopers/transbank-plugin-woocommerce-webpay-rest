cd6e69895d00b18ba619c43260324084f0e08478
Matias Vasquez <m.vasquezac@gmail.com>
2024-01-12 18:03:02 -0300

    refactor: rename logs dir constant

--- plugin/src/Helpers/TbkFactory.php
+++ plugin/src/Helpers/TbkFactory.php
@@ -5,13 +5,16 @@ namespace Transbank\WooCommerce\WebpayRest\Helpers;
 use Transbank\Plugin\Helpers\PluginLogger;
 use Transbank\Plugin\Model\LogConfig;
 
-define('Transbank_webpay_Rest_Webpay_UPLOADS', untrailingslashit(wp_upload_dir()['basedir'] . '/transbank_webpay_plus_rest'));
+define(
+    'TRANSBANK_WEBPAY_REST_UPLOADS',
+    untrailingslashit(wp_upload_dir()['basedir'] . '/transbank_webpay_plus_rest')
+);
 
 class TbkFactory
 {
     public static function createLogger()
     {
-        $config = new LogConfig(Transbank_webpay_Rest_Webpay_UPLOADS .'/logs');
+        $config = new LogConfig(TRANSBANK_WEBPAY_REST_UPLOADS .'/logs');
         return new PluginLogger($config);
     }
 }
