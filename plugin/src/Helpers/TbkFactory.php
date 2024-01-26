1aab0dddcd09c38b425123004247155fa633d872
Miguel Vargas <mvarlic@gmail.com>
2024-01-26 16:36:19 -0500

    feat: add factory methods for webpayplus and oneclick class

--- plugin/src/Helpers/TbkFactory.php
+++ plugin/src/Helpers/TbkFactory.php
@@ -3,7 +3,10 @@
 namespace Transbank\WooCommerce\WebpayRest\Helpers;
 
 use Transbank\Plugin\Helpers\PluginLogger;
+use Transbank\Plugin\Helpers\StringUtils;
 use Transbank\Plugin\Model\LogConfig;
+use Transbank\WooCommerce\WebpayRest\OneclickTransbankSdk;
+use Transbank\WooCommerce\WebpayRest\WebpayplusTransbankSdk;
 
 define(
     'TRANSBANK_WEBPAY_REST_UPLOADS',
@@ -17,5 +20,31 @@ class TbkFactory
         $config = new LogConfig(TRANSBANK_WEBPAY_REST_UPLOADS .'/logs');
         return new PluginLogger($config);
     }
-}
 
+    public static function createWebpayplusTransbankSdk()
+    {
+        return new WebpayplusTransbankSdk(static::createLogger(),
+            static::getVar('webpay_rest_environment'),
+            static::getVar('webpay_rest_commerce_code'),
+            static::getVar('webpay_rest_api_key')
+        );
+    }
+
+    public static function createOneclickTransbankSdk()
+    {
+        return new OneclickTransbankSdk(static::createLogger(),
+            static::getVar('environment'),
+            static::getVar('commerce_code'),
+            static::getVar('api_key'),
+            static::getVar('child_commerce_code')
+        );
+    }
+
+    private static function getVar($name){
+        $value = get_option($name);
+        if (!StringUtils::isNotBlankOrNull($value)){
+            return '';
+        }
+        return $value;
+    }
+}
