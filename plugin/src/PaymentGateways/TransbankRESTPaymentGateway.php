94453daf88963ce3d8551f7bd0e0d80a5e8593f8
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-02-07 11:12:13 -0300

    feat: format refund response

--- plugin/src/PaymentGateways/TransbankRESTPaymentGateway.php
+++ plugin/src/PaymentGateways/TransbankRESTPaymentGateway.php
@@ -3,28 +3,43 @@
 namespace Transbank\WooCommerce\WebpayRest\PaymentGateways;
 
 use WC_Order;
+use DateTime;
+use DateTimeZone;
 
 trait TransbankRESTPaymentGateway
 {
     /**
-     * @param  $response
+     * @param $response
      * @param WC_Order $order
      * @param $amount
-     * @param $jsonResponse
      */
     public function addRefundOrderNote($response, WC_Order $order, $amount)
     {
         $type = $response->getType() === 'REVERSED' ? 'Reversa' : 'Anulación';
         $amountFormatted = '$'.number_format($amount, 0, ',', '.');
-        $balanceFormatted = '$'.number_format($response->getBalance(), 0, ',', '.');
-        $note = "<div class='transbank_response_note'>
-          <p><h3>Reembolso exitoso</h3></p><br />
+        $commonFields = "<div class='transbank_response_note'>
+            <h3>Reembolso exitoso</h3>
+            <strong>Tipo:</strong> {$type}
+            <strong>Monto reembolso:</strong> {$amountFormatted}";
+        
+        if($type === 'Reversa') {
+            $note = "{$commonFields}
+            </div>";
+        }
+        else {
+            $balanceFormatted = '$'.number_format($response->getBalance(), 0, ',', '.');
+            $transactionDate = $response->getAuthorizationDate();
+            $utcDate = new DateTime($transactionDate, new DateTimeZone('UTC'));
+            $utcDate->setTimeZone(new DateTimeZone(wc_timezone_string()));
+            $formattedDate = $utcDate->format('d-m-Y / H:i:s');
 
-          <strong>Medio de pago:</strong> Webpay Oneclick<br />
-          <strong>Tipo:</strong> {$type}<br />
-          <strong>Monto devuelto:</strong> {$amountFormatted}<br />
-          <strong>Balance:</strong> {$balanceFormatted}<br /><br /><br />
-        </div>";
+            $note = "{$commonFields}
+                <strong>Balance:</strong> {$balanceFormatted}
+                <strong>Fecha:</strong> {$formattedDate}
+                <strong>Código autorización:</strong> {$response->getAuthorizationCode()}
+                <strong>Código de respuesta:</strong> {$response->getResponseCode()}
+            </div>";
+        }
 
         $order->add_order_note($note);
     }
