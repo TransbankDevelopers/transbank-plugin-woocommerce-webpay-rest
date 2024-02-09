868f572d0c5b317fd69ef1b23c7007007ea48d2a
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-02-09 16:10:58 -0300

    docs: add documentation for TbkResponseUtil

--- plugin/src/Helpers/TbkResponseUtil.php
+++ plugin/src/Helpers/TbkResponseUtil.php
@@ -2,23 +2,55 @@
 
 use Transbank\Plugin\Helpers\TbkConstants;
 
-class TbkResponseUtil {
-    public static function getPaymentType(string $paymentType) {
-        return TbkConstants::PAYMENT_TYPE[$paymentType]?? $paymentType;
+/**
+ * Utility class for handling Transbank responses.
+ */
+class TbkResponseUtil
+{
+    /**
+     * Get the payment type from its code.
+     *
+     * @param string $paymentType The code of the payment type.
+     * @return string The corresponding payment type.
+     */
+    public static function getPaymentType(string $paymentType)
+    {
+        return TbkConstants::PAYMENT_TYPE[$paymentType] ?? $paymentType;
     }
 
-    public static function getInstallmentType(string $paymentType) {
-        return TbkConstants::INSTALLMENT_TYPE[$paymentType]?? $paymentType;
+    /**
+     * Get the installment type from the payment type response.
+     *
+     * @param string $paymentType The code of the installment type.
+     * @return string The corresponding installment type.
+     */
+    public static function getInstallmentType(string $paymentType)
+    {
+        return TbkConstants::INSTALLMENT_TYPE[$paymentType] ?? $paymentType;
     }
 
-    public static function getStatus(string $status) {
-        return TbkConstants::STATUS_DESCRIPTION[$status]?? $status;
+    /**
+     * Get the transaction status description from response status.
+     *
+     * @param string $status The code of the transaction status.
+     * @return string The description of the corresponding transaction status.
+     */
+    public static function getStatus(string $status)
+    {
+        return TbkConstants::STATUS_DESCRIPTION[$status] ?? $status;
     }
 
-    public static function getAccountingDate(string $accountingDate) {
+    /**
+     * Get the formatted accounting date from response.
+     *
+     * @param string $accountingDate The accounting date in 'md' format.
+     * @return string The accounting date in 'mm-dd' format.
+     */
+    public static function getAccountingDate(string $accountingDate)
+    {
         $date = DateTime::createFromFormat('md', $accountingDate);
 
-        if(!$date) {
+        if (!$date) {
             return $accountingDate;
         }
 
