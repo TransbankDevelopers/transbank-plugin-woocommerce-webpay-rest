f24a5db5dda2bc957ee3f2bb59e5c8679a35272c
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-02-09 15:47:55 -0300

    feat: add debit payment type in TbkConstant

--- plugin/shared/Helpers/TbkConstants.php
+++ plugin/shared/Helpers/TbkConstants.php
@@ -59,9 +59,10 @@ class TbkConstants
     ];
 
     const PAYMENT_TYPE_CREDIT = "Crédito";
+    const PAYMENT_TYPE_DEBIT = "Débito";
 
     const PAYMENT_TYPE = [
-        "VD" => "Débito",
+        "VD" => self::PAYMENT_TYPE_DEBIT,
         "VN" => self::PAYMENT_TYPE_CREDIT,
         "VC" => self::PAYMENT_TYPE_CREDIT,
         "SI" => self::PAYMENT_TYPE_CREDIT,
