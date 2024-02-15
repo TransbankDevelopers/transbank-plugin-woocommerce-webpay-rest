bc249968d4530c35c5472adaef805e103c9d51da
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-02-15 01:20:56 -0300

    feat: implement Template class to render views

--- plugin/src/Controllers/ThankYouPageController.php
+++ plugin/src/Controllers/ThankYouPageController.php
@@ -9,14 +9,18 @@ use Transbank\WooCommerce\WebpayRest\Helpers\TbkResponseUtil;
 use Transbank\WooCommerce\WebpayRest\Models\Transaction;
 use Transbank\WooCommerce\WebpayRest\PaymentGateways\WC_Gateway_Transbank_Oneclick_Mall_REST;
 use Transbank\WooCommerce\WebpayRest\PaymentGateways\WC_Gateway_Transbank_Webpay_Plus_REST;
+use Transbank\WooCommerce\WebpayRest\Utils\Template;
 use WC_Order;
 
 class ThankYouPageController
 {
     private $logger;
+    private Template $template;
+
     public function __construct() {
         $logger = TbkFactory::createLogger();
         $this->logger = $logger;
+        $this->template = new Template();
     }
 
     public function show($orderId)
@@ -37,27 +41,16 @@ class ThankYouPageController
             return;
         }
 
-        // Transacción aprobada
-        wc_print_notice(__('Transacción aprobada', 'transbank_wc_plugin'), 'success');
-        $finalResponse = json_decode($webpayTransaction->transbank_response);
+        $commitResponse = json_decode($webpayTransaction->transbank_response);
 
-        if ($webpayTransaction->product == Transaction::PRODUCT_WEBPAY_ONECLICK) {
-            $firstTransaction = $finalResponse->details[0] ?? null;
-            $responseCode = $firstTransaction->responseCode ?? null;
-            $status = $firstTransaction->status ?? null;
-            $paymentTypeCode = $firstTransaction->paymentTypeCode;
-            $responseTitle = ($responseCode === 0 && $status === 'AUTHORIZED') ? 'Transacción Aprobada' : 'Transacción Rechazada';
-            $dateAccepted = new DateTime($finalResponse->transactionDate ?? null, new DateTimeZone('UTC'));
-            $dateAccepted->setTimeZone(new DateTimeZone(wc_timezone_string()));
-            $paymentType = TbkResponseUtil::getPaymentType($paymentTypeCode);
-            $installmentType = TbkResponseUtil::getInstallmentType($paymentTypeCode);
-            require_once __DIR__.'/../../templates/public/order/order-summary-oneclick.php';
-
+        if($webpayTransaction->product == Transaction::PRODUCT_WEBPAY_ONECLICK) {
+            $formattedResponse = TbkResponseUtil::getOneclickFormattedResponse($commitResponse);
+            $this->template->render('public/order/order-summary.php', $formattedResponse);
             return;
         }
-        $data = (new ResponseController([]))->getTransactionDetails($finalResponse);
-        list($authorizationCode, $amount, $sharesNumber, $transactionResponse, $installmentType, $date_accepted, $sharesAmount, $paymentType) = $data;
-        require_once __DIR__.'/../../templates/public/order/order-summary.php';
+
+        $formattedResponse = TbkResponseUtil::getWebpayFormattedResponse($commitResponse);
+        $this->template->render('public/order/order-summary.php', $formattedResponse);
     }
 
     private function isValidPaymentGateway($paymentMethod): bool {
