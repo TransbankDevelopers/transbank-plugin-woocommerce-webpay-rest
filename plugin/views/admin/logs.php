406cf7d0d92e56cebca723e157296c0e7e42dd80
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-02-26 17:53:31 -0300

    style: format logs file

--- plugin/views/admin/logs.php
+++ plugin/views/admin/logs.php
@@ -1,6 +1,7 @@
 <?php
 
 use Transbank\WooCommerce\WebpayRest\Helpers\TbkFactory;
+
 if (!defined('ABSPATH')) {
     return;
 }
@@ -16,64 +17,60 @@ $folderHasLogs = $resume['length'] > 0;
         <div id="tbk_information_logs" style="margin-bottom: 30px;">
             <div class="tbk-plugin-info-container logs" id="div_status_logs">
                 <div class="info-column">
-                    <div title="Informa si actualmente se guarda la información de cada compra mediante Webpay"
-                         class="label label-info">?
+                    <div title="Informa si actualmente se guarda la información de cada compra mediante Webpay" class="label label-info">?
                     </div>
                 </div>
                 <div class="info-column">
-                    <span class="highlight-text"> Estado de Registros:  </span>
+                    <span class="highlight-text"> Estado de Registros: </span>
                 </div>
                 <div class="info-column">
                     <span id="action_txt" class="label label-success">Registro activado</span>
                 </div>
             </div>
-            <div class="tbk-plugin-info-container logs" id="div_logs_path" >
+            <div class="tbk-plugin-info-container logs" id="div_logs_path">
                 <div class="info-column">
-                    <div title="Carpeta en el servidor en donde se guardan los archivos con la informacón de cada compra mediante Webpay"
-                         class="label label-info">?
+                    <div title="Carpeta en el servidor en donde se guardan los archivos con la informacón de cada compra mediante Webpay" class="label label-info">?
                     </div>
                 </div>
                 <div class="info-column-plugin log">
-                    <span class="highlight-text"> Directorio de registros:  </span>
+                    <span class="highlight-text"> Directorio de registros: </span>
                 </div>
                 <div class="info-column-plugin log">
-                <span class="label">
-                    <?php echo $resume['dir']; ?>
-                </span>
+                    <span class="label">
+                        <?php echo $resume['dir']; ?>
+                    </span>
                 </div>
             </div>
             <div class="tbk-plugin-info-container logs" id="div_logs_number">
                 <div class="info-column">
-                    <div title="Cantidad de archivos que guardan la información de cada compra mediante Webpay"
-                         class="label label-info">?
+                    <div title="Cantidad de archivos que guardan la información de cada compra mediante Webpay" class="label label-info">?
                     </div>
                 </div>
                 <div class="info-column-plugin log">
-                    <span class="highlight-text"> Cantidad de Registros en Directorio:  </span>
+                    <span class="highlight-text"> Cantidad de Registros en Directorio: </span>
                 </div>
                 <div class="info-column-plugin log">
-                <span class="label">
-                   <?php echo $resume['length']; ?>
-                </span>
+                    <span class="label">
+                        <?php echo $resume['length']; ?>
+                    </span>
                 </div>
             </div>
             <?php if ($folderHasLogs) { ?>
                 <div class="tbk-plugin-info-container logs" id="div_logs_list">
                     <div class="info-column">
-                        <div title="Lista los archivos que guardan la información de cada compra mediante Webpay"
-                            class="label label-info">?
+                        <div title="Lista los archivos que guardan la información de cada compra mediante Webpay" class="label label-info">?
                         </div>
                     </div>
                     <div class="info-column-plugin log">
-                        <span class="highlight-text"> Listado de Registros Disponibles:  </span>
+                        <span class="highlight-text"> Listado de Registros Disponibles: </span>
                     </div>
                     <div class="info-column-plugin log">
                         <span class="label">
                             <?php
-                                foreach ($resume['logs'] as $index) {
-                                    $str = '<li>'.$index['filename'].'</li>';
-                                    echo $str;
-                                }
+                            foreach ($resume['logs'] as $index) {
+                                $str = '<li>' . $index['filename'] . '</li>';
+                                echo $str;
+                            }
                             ?>
                         </span>
                     </div>
@@ -83,50 +80,47 @@ $folderHasLogs = $resume['length'] > 0;
 
         <?php if ($folderHasLogs) { ?>
             <h3 class="tbk_title_h3">Últimos Registros</h3>
-            <div id="tbk-last-logs" >
+            <div id="tbk-last-logs">
                 <div class="tbk-plugin-info-container logs" id="div_last_log">
                     <div class="info-column">
-                        <div title="Nombre del útimo archivo de registro creado"
-                            class="label label-info">?
+                        <div title="Nombre del útimo archivo de registro creado" class="label label-info">?
                         </div>
                     </div>
                     <div class="info-column-plugin">
                         <span class="highlight-text"> Último Documento: </span>
                     </div>
                     <div class="info-column-plugin">
-                            <span class="label">
-                                <?php echo $lastLog['filename'] ?? '-'; ?>
-                            </span>
+                        <span class="label">
+                            <?php echo $lastLog['filename'] ?? '-'; ?>
+                        </span>
                     </div>
                 </div>
                 <div class="tbk-plugin-info-container logs" id="div_size_log">
                     <div class="info-column">
-                        <div title="Peso del último archivo de registro creado"
-                            class="label label-info">?
+                        <div title="Peso del último archivo de registro creado" class="label label-info">?
                         </div>
                     </div>
                     <div class="info-column-plugin">
                         <span class="highlight-text"> Peso del Documento: </span>
                     </div>
                     <div class="info-column-plugin">
-                            <span class="label">
-                                <?php echo $lastLog['size'] ?? '-'; ?>
-                            </span>
+                        <span class="label">
+                            <?php echo $lastLog['size'] ?? '-'; ?>
+                        </span>
                     </div>
                 </div>
                 <div class="tbk-plugin-info-container logs" id="div_lines_logs">
                     <div class="info-column">
-                        <div title="Cantidad de líneas que posee el último archivo de registro creado"
-                            class="label label-info">?
+                        <div title="Cantidad de líneas que posee el último archivo de registro creado" class="label label-info">?
                         </div>
                     </div>
                     <div class="info-column-plugin">
                         <span class="highlight-text"> Cantidad de Líneas: </span>
                     </div>
                     <div class="info-column-plugin">
-                            <span class="label">
-                                <?php echo $lastLog['lines'] ?? '-'; ?>
-                            </span>
+                        <span class="label">
+                            <?php echo $lastLog['lines'] ?? '-'; ?>
+                        </span>
                     </div>
                 </div>
             </div>
