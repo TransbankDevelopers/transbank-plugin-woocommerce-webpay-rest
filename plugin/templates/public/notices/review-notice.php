d35c4fe7d3dde7aa1118f43d018170fe3636c36a
Matias Vasquez <m.vasquezac@gmail.com>
2024-03-01 17:25:11 -0300

    feat: add template for review notice

---
+++ plugin/templates/public/notices/review-notice.php
@@ -0,0 +1,16 @@
+<div class="notice notice-info is-dismissible tbk-review-container">
+    <div>
+        <img src="<?= $tbkLogo ?>" height="70px" alt="Transbank logo" />
+    </div>
+    <div class="tbk-review-text">
+        <p class="tbk-review-title">Tu opinión es importante para nosotros</p>
+        <p>¿Podrías tomarte un momento para dejarnos una reseña en el repositorio de WordPress?
+        Solo te tomará un par de minutos
+        y nos ayudará a seguir mejorando y llegar a más personas como tú.</p>
+    </div>
+    <div class="tbk-review-btn">
+    <a href="https://wordpress.org/support/plugin/transbank-webpay-plus-rest/reviews/#new-post"
+        target="_blank" class="tbk-review-btn-link">Dejar reseña</a>
+    </div>
+
+</div>
