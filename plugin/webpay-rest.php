b389dc322331ecd01301ff095ec118c0454f150e
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-05-29 15:14:29 -0400

    chore: update woocommerce tested up version

--- plugin/webpay-rest.php
+++ plugin/webpay-rest.php
@@ -32,7 +32,7 @@ if (!defined('ABSPATH')) {
  * Author: TransbankDevelopers
  * Author URI: https://www.transbank.cl
  * WC requires at least: 7.0
- * WC tested up to: 8.5.1
+ * WC tested up to: 8.9.1
  */
 
 require_once plugin_dir_path(__FILE__).'vendor/autoload.php';
