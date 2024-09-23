2bd76cf4a06ac0016b54481ea706cb39c5acb96a
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-09-23 16:43:56 -0300

    refactor: improve get status flow

--- plugin/src/Controllers/TransactionStatusController.php
+++ plugin/src/Controllers/TransactionStatusController.php
@@ -24,59 +24,69 @@ class TransactionStatusController
         $token = filter_input(INPUT_POST, 'token', FILTER_DEFAULT);
         $token = htmlspecialchars($token, ENT_QUOTES, 'UTF-8');
 
-        $transaction = Transaction::getApprovedByOrderId($orderId);
-        if (!$transaction) {
-            wp_send_json([
-                'message' => 'No hay transacciones webpay aprobadas para esta orden',
-            ], 401);
-        }
-
-        if ($transaction->product == Transaction::PRODUCT_WEBPAY_ONECLICK) {
-            if ($transaction->buy_order !== $buyOrder) {
+        try {
+            $transaction = Transaction::getApprovedByOrderId($orderId);
+            if (!$transaction) {
                 wp_send_json([
-                    'message' => 'El buy_order enviado y el buy_order de la transacción no coinciden',
+                    'message' => 'No hay transacciones webpay aprobadas para esta orden',
                 ], 401);
             }
-            $oneclickTransbankSdk = TbkFactory::createOneclickTransbankSdk();
-            $status = $oneclickTransbankSdk->status($orderId, $buyOrder);
-            $statusArray = json_decode(json_encode($status), true);
-            $firstDetail = json_decode(json_encode($status->getDetails()[0]), true);
 
-            $response = array_merge($statusArray, $firstDetail);
-            $formattedDate = TbkResponseUtil::transactionDateToLocalDate($status->getTransactionDate());
-            $response['transactionDate'] = $formattedDate;
-            unset($response['details']);
-            wp_send_json([
-                'product' => $transaction->product,
-                'status'  => $response,
-                'raw'     => $status,
-            ]);
+            if ($transaction->product == Transaction::PRODUCT_WEBPAY_ONECLICK) {
+                if ($transaction->buy_order !== $buyOrder) {
+                    wp_send_json([
+                        'message' => 'El buy_order enviado y el buy_order de la transacción no coinciden',
+                    ], 401);
+                }
 
-            return;
-        }
+                return wp_send_json($this->getStatusForOneclickTransaction($orderId, $buyOrder));
+            }
 
-        if ($transaction->token !== $token) {
-            wp_send_json([
-                'message' => 'El token enviado y el token de la transacción no coinciden',
-            ], 401);
-        }
+            if ($transaction->token !== $token) {
+                wp_send_json([
+                    'message' => 'El token enviado y el token de la transacción no coinciden',
+                ], 401);
+            }
 
-        try {
-            $webpayplusTransbankSdk = TbkFactory::createWebpayplusTransbankSdk();
-            $resp = $webpayplusTransbankSdk->status($transaction->order_id, $transaction->token);
-            $formattedDate = TbkResponseUtil::transactionDateToLocalDate($resp->getTransactionDate());
-            $modifiedResponse = clone $resp;
-            $modifiedResponse->setTransactionDate($formattedDate);
-            wp_send_json([
-                'product' => $transaction->product,
-                'status'  => $modifiedResponse,
-                'raw'     => $resp,
-            ]);
+            return wp_send_json($this->getStatusForWebpayTransaction($orderId, $token));
         } catch (\Exception $e) {
             wp_send_json([
                 'message' => $e->getMessage(),
             ], 422);
         }
     }
-}
 
+    private function getStatusForWebpayTransaction(string $orderId, string $token)
+    {
+        $webpayplusTransbankSdk = TbkFactory::createWebpayplusTransbankSdk();
+        $resp = $webpayplusTransbankSdk->status($orderId, $token);
+        $formattedDate = TbkResponseUtil::transactionDateToLocalDate($resp->getTransactionDate());
+        $modifiedResponse = clone $resp;
+        $modifiedResponse->setTransactionDate($formattedDate);
+
+        return [
+            'product' => Transaction::PRODUCT_WEBPAY_PLUS,
+            'status'  => $modifiedResponse,
+            'raw'     => $resp,
+        ];
+    }
+
+    private function getStatusForOneclickTransaction(string $orderId, string $buyOrder)
+    {
+        $oneclickTransbankSdk = TbkFactory::createOneclickTransbankSdk();
+        $status = $oneclickTransbankSdk->status($orderId, $buyOrder);
+        $statusArray = json_decode(json_encode($status), true);
+        $firstDetail = json_decode(json_encode($status->getDetails()[0]), true);
+
+        $response = array_merge($statusArray, $firstDetail);
+        $formattedDate = TbkResponseUtil::transactionDateToLocalDate($status->getTransactionDate());
+        $response['transactionDate'] = $formattedDate;
+        unset($response['details']);
+
+        return [
+            'product' => Transaction::PRODUCT_WEBPAY_ONECLICK,
+            'status'  => $response,
+            'raw'     => $status,
+        ];
+    }
+}
