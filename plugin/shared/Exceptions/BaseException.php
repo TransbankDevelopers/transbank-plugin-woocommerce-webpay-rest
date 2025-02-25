30a9b1a607ddb623c7fa32bb8921df52c72a27b0
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2025-02-25 12:14:04 -0300

    feat: use throwable interface in base exception

--- plugin/shared/Exceptions/BaseException.php
+++ plugin/shared/Exceptions/BaseException.php
@@ -6,7 +6,7 @@ use Transbank\Plugin\Helpers\ExceptionConstants;
 
 class BaseException extends \Exception
 {
-    public function __construct($message, \Exception $previous = null) {
+    public function __construct($message, \Throwable $previous = null) {
         parent::__construct($message, ExceptionConstants::DEFAULT_CODE, $previous);
     }
 }
