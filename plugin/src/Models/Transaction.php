3225642f2c0e378104d2e1a752d9f65e498ce4ae
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2025-03-10 13:00:30 -0300

    feat: add option to order row in select for transactions

--- plugin/src/Models/Transaction.php
+++ plugin/src/Models/Transaction.php
@@ -21,11 +21,13 @@ class Transaction extends BaseModel
     /**
      * Get transactions by custom conditions.
      *
-     * @param array $conditions Key-value pairs of column names and values.
+     * @param array $conditions         Key-value pairs of column names and values.
+     * @param string|string[] $orderBy  Column name or an array of column names to order by.
+     * @param string $orderDirection    Order direction ('ASC' or 'DESC').
      *
-     * @return array|null Transaction data.
+     * @return array|null Transaction objects array.
      */
-    private static function getTransactionsByConditions(array $conditions): ?array
+    private static function getTransactionsByConditions(array $conditions, string $orderBy = '', string $orderDirection = 'DESC'): ?array
     {
         global $wpdb;
         $tableName = static::getTableName();
@@ -37,6 +39,17 @@ class Transaction extends BaseModel
 
         $whereSql = implode(' AND ', $whereClauses);
         $sql = "SELECT * FROM {$tableName} WHERE {$whereSql}";
+
+        if (!empty($orderBy)) {
+            if (is_array($orderBy)) {
+                $orderBy = implode(", ", array_map('esc_sql', $orderBy));
+            } else {
+                $orderBy = esc_sql($orderBy);
+            }
+            $orderDirection = strtoupper($orderDirection) === 'DESC' ? 'DESC' : 'ASC';
+            $sql .= " ORDER BY {$orderBy} {$orderDirection}";
+        }
+
         return $wpdb->get_results($sql);
     }
 
@@ -49,7 +62,7 @@ class Transaction extends BaseModel
      */
     public static function getByOrderId($orderId): ?object
     {
-        return static::getTransactionsByConditions(['order_id' => $orderId])[0] ?? null;
+        return static::getTransactionsByConditions(['order_id' => $orderId], 'id')[0] ?? null;
     }
 
     /**
