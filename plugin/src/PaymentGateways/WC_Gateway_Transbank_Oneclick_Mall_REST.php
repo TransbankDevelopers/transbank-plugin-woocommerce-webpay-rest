e943d0c98edfb9c5c6f2feb1b9f6a15aacaf2d50
Mauricio Astudillo <m.astudillo.toledo@outlook.cl>
2025-03-26 19:20:24 -0300

    feat: improve product activation option

--- plugin/src/PaymentGateways/WC_Gateway_Transbank_Oneclick_Mall_REST.php
+++ plugin/src/PaymentGateways/WC_Gateway_Transbank_Oneclick_Mall_REST.php
@@ -520,8 +520,9 @@ class WC_Gateway_Transbank_Oneclick_Mall_REST extends WC_Payment_Gateway_CC
 
         $this->form_fields = [
             'enabled' => [
-                'title'   => __('Activar/Desactivar', 'transbank_wc_plugin'),
+                'title'   => __('Activo', 'transbank_wc_plugin'),
                 'type'    => 'checkbox',
+                'label'   => " ",
                 'default' => 'no',
             ],
             'environment' => [
