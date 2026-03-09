b130118c0fbe147d23467217d54a8a50826fb2a0
Luis Guananga <luis.guananga@continuumhq.com>
2026-03-08 21:05:41 -0500

    refactor: isolate assets and modularize one click page architecture
    Refactors the admin one click page to improve asset isolation and architectural structure.
    
    Modularized the previous monolithic admin.js into feature-based modules.
    Limited event listeners to the plugin container.

---
+++ plugin/assets/src/admin/pages/buy-order-one-click/index.js
@@ -0,0 +1,40 @@
+import { BuyOrderValidator } from "../../modules/buy-format-order/buyOrderValidator";
+import { BuyOrderPreviewGenerator } from "../../modules/buy-format-order/buyOrderPreviewGenerator";
+import { BuyOrderService } from "../../modules/buy-format-order/buyOrderService";
+import { BuyOrderFormatComponent } from "../../modules/buy-format-order/buyOrderFormatComponent";
+import { DEFAULT_BUY_ORDER_FORMATS, SELECTORS } from "../../modules/buy-format-order/utils/constants";
+import { DOMUtils } from "../../utils/dom";
+
+DOMUtils.ready(() => {
+    const oneClicInput = document.querySelector(SELECTORS.ONECLICK_BUY_ORDER_INPUT);
+    const oneClickChildInput = document.querySelector(SELECTORS.ONECLICK_CHILD_BUY_ORDER_INPUT);
+
+    if (!oneClicInput || !oneClickChildInput)
+        return;
+
+    const validator = new BuyOrderValidator();
+    const preview = new BuyOrderPreviewGenerator();
+    const service = new BuyOrderService(validator, preview);
+    const component = new BuyOrderFormatComponent(service);
+
+    component.attach(SELECTORS.ONECLICK_BUY_ORDER_INPUT, DEFAULT_BUY_ORDER_FORMATS.ONECLICK_PARENT, {
+        addHelpText: false,
+        isOneClick: true,
+        getOtherFormat: () => ({
+            format: oneClickChildInput?.value ?? null,
+            customMessage: 'El formato de orden de compra principal no puede ser igual al formato de orden de compra hija.'
+        })
+    });
+
+    component.attach(
+        SELECTORS.ONECLICK_CHILD_BUY_ORDER_INPUT,
+        DEFAULT_BUY_ORDER_FORMATS.ONECLICK_CHILD,
+        {
+            addHelpText: true,
+            isOneClick: true,
+            getOtherFormat: () => ({
+                format: oneClicInput?.value ?? null,
+            }),
+        },
+    );
+});
