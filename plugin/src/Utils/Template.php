c5996177457a4c9e5780adb46c9bca195a1b74c6
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2026-03-24 19:13:56 -0300

    feat: refine template path independently of plugin folder name

--- plugin/src/Utils/Template.php
+++ plugin/src/Utils/Template.php
@@ -3,8 +3,14 @@
 namespace Transbank\WooCommerce\WebpayRest\Utils;
 
 class Template {
-    const TEMPLATE_PATH = WP_PLUGIN_DIR.'/transbank-webpay-plus-rest/templates/';
+    private const TEMPLATE_DIR = '/templates/';
+
+    private static function getTemplatePath(): string
+    {
+        return dirname(dirname(__DIR__)) . self::TEMPLATE_DIR;
+    }
+
     public function render(string $name, array $parameters): void {
-        wc_get_template( $name, $parameters, null, self::TEMPLATE_PATH );
+        wc_get_template($name, $parameters, null, self::getTemplatePath());
     }
 }
